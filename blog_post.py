f42908a78c4d7126012fbd45b81ce1627bce29cf
Ignacio Buioli <ibuioli@gmail.com>
2022-10-12 20:27:06 -0300

    fix: blog_post

--- blog_post.py
+++ blog_post.py
@@ -15,7 +15,7 @@ urlTo = 'http://localhost:8069'         # Odoo To URL
 
 model = 'blog.post'                     # Modelo de Odoo a migrar
 
-valsFrom = {'fields': ['blog_id', 'visits', 'name', 'subtitle', 'author_id', 'create_date', 'post_date', 'published_date', 'write_date', 'tag_ids']}
+valsFrom = {'fields': ['blog_id', 'visits', 'name', 'subtitle', 'author_id', 'create_date', 'post_date', 'content', 'published_date', 'write_date', 'tag_ids', 'website_meta_keywords', 'website_meta_title', 'website_meta_description']}
 
 commonFrom = xmlrpclib.ServerProxy('{}/xmlrpc/2/common'.format(urlFrom))
 commonFrom.version()
@@ -36,29 +36,33 @@ for id_from in ids_from:
     blog_id_to = False
     author_id_to = False
     if from_id[0]['blog_id']:
-        blog_id_from = modelsFrom.execute_kw(dbFrom, uidFrom, pwdFrom, 'blog.blog', 'read', [from_id[0]['blog_id']], {'fields': 'name'})
-        blog_id_to = modelsFrom.execute_kw(dbTo, uidTo, pwdTo, 'blog.blog', 'read', [[('name', '=', blog_id_from[0]['name'])]], {'fields': 'id'})[0]['id']
+        blog_id_from = modelsFrom.execute_kw(dbFrom, uidFrom, pwdFrom, 'blog.blog', 'read', [from_id[0]['blog_id'][0]], {'fields': ['name']})
+        blog_id_to = modelsTo.execute_kw(dbTo, uidTo, pwdTo, 'blog.blog', 'search', [[('name', '=', blog_id_from[0]['name'])]])[0]
     if from_id[0]['author_id']:
-        author_id_from = modelsFrom.execute_kw(dbFrom, uidFrom, pwdFrom, 'res.partner', 'read', [from_id[0]['author_id']], {'fields': 'name'})
-        author_id_to = modelsFrom.execute_kw(dbTo, uidTo, pwdTo, 'res.partner', 'read', [[('name', '=', author_id_from[0]['name'])]], {'fields': 'id'})[0]['id']
-    
+        author_id_from = modelsFrom.execute_kw(dbFrom, uidFrom, pwdFrom, 'res.partner', 'read', [from_id[0]['author_id'][0]], {'fields': ['name']})
+        author_id_to = modelsTo.execute_kw(dbTo, uidTo, pwdTo, 'res.partner', 'search', [[('name', '=', author_id_from[0]['name'])]])[0]
+
     tags = []
 
     for tag_id in from_id[0]['tag_ids']:
-        tag_id_from = modelsFrom.execute_kw(dbFrom, uidFrom, pwdFrom, 'blog.tag', 'read', [tag_id], {'fields': 'name'})
-        tag_id_to = modelsFrom.execute_kw(dbTo, uidTo, pwdTo, 'blog.tag', 'read', [[('name', '=', tag_id_from[0]['name'])]], {'fields': 'id'})
-        tags.append(tag_id_to[0]['id'])
+        tag_id_from = modelsFrom.execute_kw(dbFrom, uidFrom, pwdFrom, 'blog.tag', 'read', [tag_id], {'fields': ['name']})
+        tag_id_to = modelsTo.execute_kw(dbTo, uidTo, pwdTo, 'blog.tag', 'search', [[('name', '=', tag_id_from[0]['name'])]])
+        tags.append(tag_id_to[0])
 
     to_id = modelsTo.execute_kw(dbTo, uidTo, pwdTo, model, 'create', [{
         'blog_id': blog_id_to,
         'name': from_id[0]['name'],
+        'content': from_id[0]['content'],
         'visits': from_id[0]['visits'],
-        'subject': from_id[0]['subject'],
+        'subtitle': from_id[0]['subtitle'],
         'author_id': author_id_to,
         'create_date': from_id[0]['create_date'],
         'post_date': from_id[0]['post_date'],
         'published_date': from_id[0]['published_date'],
         'write_date': from_id[0]['write_date'],
         'is_published': True,
-        'tag_ids': tags
+        'tag_ids': tags,
+        'website_meta_keywords': from_id[0]['website_meta_keywords'],
+        'website_meta_title': from_id[0]['website_meta_title'],
+        'website_meta_description': from_id[0]['website_meta_description'],
     }])
